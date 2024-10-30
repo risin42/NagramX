d909de12015ef93c45212e6389068c4fffadea81
risin42 <5331402+risin42@users.noreply.github.com>
2024-10-30 17:30:41 +0900

    fix: InternalUpdater.java

--- TMessagesProj/src/main/java/tw/nekomimi/nekogram/InternalUpdater.java
+++ TMessagesProj/src/main/java/tw/nekomimi/nekogram/InternalUpdater.java
@@ -119,11 +119,9 @@ public class InternalUpdater {
     }
 
     public static void checkUpdate(checkUpdateCallback callback) {
-        callback.apply(null, false); // disable update
-        return;
-
         AccountInstance accountInstance = AccountInstance.getInstance(UserConfig.selectedAccount);
         retrieveUpdateMetadata((metadata, err) -> {
+            callback.apply(null, false); // disable update
             if (metadata == null) {
                 callback.apply(null, err);
                 return;
