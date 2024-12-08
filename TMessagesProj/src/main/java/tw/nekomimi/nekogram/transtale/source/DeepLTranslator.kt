531a3f6858af525ff77dcc51b3693565a8d0fdf7
risin42 <5331402+risin42@users.noreply.github.com>
2024-12-08 15:24:55 +0900

    feat(refactor): DeepL Translator - Integrated a personal API key for the DeepL Translator, offering faster and more stable translations. - Removed the old DeepL Translator implementation that simulated browser requests, as it was no longer functional.

--- TMessagesProj/src/main/java/tw/nekomimi/nekogram/transtale/source/DeepLTranslator.kt
+++ TMessagesProj/src/main/java/tw/nekomimi/nekogram/transtale/source/DeepLTranslator.kt
@@ -1,26 +1,50 @@
 package tw.nekomimi.nekogram.transtale.source
 
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
+import cn.hutool.http.HttpUtil
+import org.json.JSONArray
+import org.json.JSONObject
+import org.telegram.messenger.BuildConfig;
 import org.telegram.messenger.LocaleController
 import org.telegram.messenger.R
 import tw.nekomimi.nekogram.transtale.Translator
-import tw.nekomimi.nekogram.transtale.deepl.DeepLTranslatorRaw
 
 object DeepLTranslator : Translator {
 
-    val targetLanguages = listOf("DE", "EN", "ES", "FR", "IT", "JA", "NL", "PL", "PT", "RU", "ZH")
-
-    val client = DeepLTranslatorRaw()
+    private const val API_ENDPOINT = "https://api-free.deepl.com/v2/translate"
 
     override suspend fun doTranslate(from: String, to: String, query: String): String {
 
-        if (to !in targetLanguages) {
-
+        if (to.isEmpty()) {
             throw UnsupportedOperationException(LocaleController.getString(R.string.TranslateApiUnsupported))
-
         }
 
-        return withContext(Dispatchers.IO) { client.translate(query, "auto", to) }
+        val apiKey = BuildConfig.DEEPL_API_KEY
+        if (apiKey.isBlank()) error("Missing DeepL Translate Key")
+ 
+        val textArray = JSONArray().apply {
+            put(query)
+        }
+
+        val requestBody = JSONObject().apply {
+            put("text", textArray)
+            put("target_lang", to.uppercase())
+        }.toString()
+
+        val response = HttpUtil.createPost(API_ENDPOINT)
+                .header("Authorization", "DeepL-Auth-Key $apiKey")
+                .header("Content-Type", "application/json")
+                .body(requestBody)
+                .execute()
+
+        if (response.status != 200) {
+            error("HTTP ${response.status} : ${response.body()}")
+        }
+
+        val respObj = JSONObject(response.body())
+        val respArr = respObj.getJSONArray("translations")
+
+        if (respArr.length() == 0) error("Empty translation result")
+
+        return respArr.getJSONObject(0).getString("text")
     }
 }
