d09a73878ceb66b70c829a83bc4d3176c68650af
risin42 <5331402+risin42@users.noreply.github.com>
2024-12-27 22:36:58 +0900

    refactor: remote configuration source - make it more concise, remove unnecessary bloat - https://t.me/nagramx_remote_metadata

--- TMessagesProj/src/main/java/tw/nekomimi/nekogram/helpers/remote/BaseRemoteHelper.java
+++ TMessagesProj/src/main/java/tw/nekomimi/nekogram/helpers/remote/BaseRemoteHelper.java
@@ -19,8 +19,8 @@ import org.telegram.tgnet.TLRPC;
 import java.util.ArrayList;
 
 public abstract class BaseRemoteHelper {
-    public static final int CHANNEL_METADATA_ID = 1471208507;
-    public static final String CHANNEL_METADATA_NAME = "nagram_remote_metadata";
+    public static final long CHANNEL_METADATA_ID = 2477822904L;
+    public static final String CHANNEL_METADATA_NAME = "nagramx_remote_metadata";
 
     protected static final SharedPreferences preferences = ApplicationLoader.applicationContext.getSharedPreferences("nekoremoteconfig", Activity.MODE_PRIVATE);
 
