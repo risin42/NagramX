0f9653e9779dce7421aa450d6f24be40bf27a1b8
risin42 <5331402+risin42@users.noreply.github.com>
2024-12-27 06:01:50 +0000

    chore: minor fixes

--- TMessagesProj/src/main/java/tw/nekomimi/nekogram/utils/GsonUtil.kt
+++ TMessagesProj/src/main/java/tw/nekomimi/nekogram/utils/GsonUtil.kt
@@ -3,12 +3,13 @@ package tw.nekomimi.nekogram.utils
 import com.google.gson.GsonBuilder
 import com.google.gson.JsonObject
 import com.google.gson.internal.Streams
-import com.google.gson.stream.JsonWriter
 import java.io.StringWriter
 
 object GsonUtil {
 
-    private val gson = GsonBuilder().create()
+    private val gson = GsonBuilder()
+        .setLenient()
+        .create()
 
     @JvmStatic
     fun formatObject(obj: Any?): String {
