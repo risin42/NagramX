b5428f2184a9523a3cac7c1021df302bf855fdcf
risin42 <5331402+risin42@users.noreply.github.com>
2025-01-23 06:36:40 +0000

    fix(refactor): Bing Translator
    - Formerly, Microsoft Translator

--- TMessagesProj/src/main/java/tw/nekomimi/nekogram/transtale/source/MicrosoftTranslator.kt
+++ TMessagesProj/src/main/java/tw/nekomimi/nekogram/transtale/source/MicrosoftTranslator.kt
@@ -1,95 +1,47 @@
 package tw.nekomimi.nekogram.transtale.source
 
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import org.json.JSONArray
-import org.telegram.messenger.FileLog
+import java.io.IOException
 import org.telegram.messenger.LocaleController
 import org.telegram.messenger.R
 import tw.nekomimi.nekogram.transtale.Translator
-import java.io.ByteArrayOutputStream
-import java.io.DataOutputStream
-import java.io.IOException
-import java.io.InputStream
-import java.net.HttpURLConnection
-import java.net.URL
-import java.net.URLEncoder
-import java.nio.charset.Charset
-import java.util.*
+import tw.nekomimi.nekogram.transtale.source.raw.BingTranslatorRaw
 
 object MicrosoftTranslator : Translator {
 
-    private val targetLanguages = Arrays.asList(
-            "ar", "as", "bn", "bs", "bg", "yue", "ca", "zh", "zh-Hans", "zh-Hant",
-            "hr", "cs", "da", "prs", "nl", "en", "et", "fj", "fil", "fi",
-            "fr", "de", "el", "gu", "ht", "he", "hi", "mww", "hu", "is",
-            "id", "ga", "it", "ja", "kn", "kk", "tlh", "ko", "ku", "kmr",
-            "lv", "lt", "mg", "ms", "ml", "mt", "mi", "mr", "nb", "or", "ps",
-            "fa", "pl", "pt", "pa", "otq", "ro", "ru", "sm", "sr", "sk", "sl",
-            "es", "sw", "sv", "ty", "ta", "te", "th", "to", "tr", "uk", "ur",
-            "vi", "cy", "yua")
-    private var useCN = false
+    private val targetLanguages = listOf(
+        "af", "sq", "am", "ar", "az", "bn", "bs", "bg", "ca", "zh-Hans", "zh-Hant",
+        "hr", "cs", "da", "nl", "en", "et", "fil", "fi", "fr", "de", "el", "gu",
+        "ht", "he", "hi", "hu", "is", "id", "it", "ja", "kn", "kk", "km", "ko",
+        "ku", "lo", "lv", "lt", "mg", "ms", "ml", "mt", "mr", "ne", "nb", "fa",
+        "pl", "pt", "ro", "ru", "sr-Cyrl", "sr-Latn", "sk", "sl", "es", "sw",
+        "sv", "ta", "te", "th", "tr", "uk", "ur", "vi"
+    )
+
+    private val rawTranslator = BingTranslatorRaw()
 
     override suspend fun doTranslate(from: String, to: String, query: String): String {
-        if (to !in targetLanguages) {
-            throw UnsupportedOperationException(LocaleController.getString(R.string.TranslateApiUnsupported))
+
+        var fromLang = if (from == "auto") "auto-detect" else from
+        var toLang =
+            when (to) {
+                "zh" -> "zh-Hans"
+                "zh-CN" -> "zh-Hans"
+                "zh-TW" -> "zh-Hant"
+                else -> to
+            }
+
+        if (toLang.lowercase() !in targetLanguages.map { it.lowercase() }) {
+            throw UnsupportedOperationException(LocaleController.getString(R.string.TranslateApiUnsupported) + " " + to)
         }
 
-        return withContext(Dispatchers.IO) {
-            val param = "fromLang=auto-detect&text=" + URLEncoder.encode(query, "UTF-8") +
-                    "&to=" + to
-            val response = request(param)
-            val jsonObject = JSONArray(response).getJSONObject(0)
-            if (!jsonObject.has("translations")) {
-                throw IOException(response)
-            }
-            val array = jsonObject.getJSONArray("translations")
-            array.getJSONObject(0).getString("text")
+        if (query.length > 3000) {
+            error("Text length exceeds the limit of 3000 characters. text length: " + query.length);
+        }
+
+        try {
+            return rawTranslator.translate(query, fromLang, toLang)
+        } catch (e: IOException) {
+            error(e.message ?: "Failed to translate")
         }
     }
-
-    private fun request(param: String): String {
-        val httpConnectionStream: InputStream
-        val downloadUrl = URL("https://" + (if (useCN) "cn" else "www") + ".bing.com/ttranslatev3")
-        val httpConnection = downloadUrl.openConnection() as HttpURLConnection
-        httpConnection.addRequestProperty("User-Agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 10_0 like Mac OS X) AppleWebKit/602.1.38 (KHTML, like Gecko) Version/10.0 Mobile/14A5297c Safari/602.1")
-        httpConnection.connectTimeout = 1000
-        //httpConnection.setReadTimeout(2000);
-        httpConnection.requestMethod = "POST"
-        httpConnection.doOutput = true
-        httpConnection.instanceFollowRedirects = false
-        val dataOutputStream = DataOutputStream(httpConnection.outputStream)
-        val t = param.toByteArray(Charset.defaultCharset())
-        dataOutputStream.write(t)
-        dataOutputStream.flush()
-        dataOutputStream.close()
-        httpConnection.connect()
-        if (httpConnection.responseCode != HttpURLConnection.HTTP_OK) {
-            if (httpConnection.responseCode == HttpURLConnection.HTTP_MOVED_TEMP) {
-                useCN = !useCN
-                FileLog.e("Move to " + if (useCN) "cn" else "www")
-                return request(param)
-            }
-            httpConnectionStream = httpConnection.errorStream
-        } else {
-            httpConnectionStream = httpConnection.inputStream
-        }
-        val outbuf = ByteArrayOutputStream()
-        val data = ByteArray(1024 * 32)
-        while (true) {
-            val read = httpConnectionStream.read(data)
-            if (read > 0) {
-                outbuf.write(data, 0, read)
-            } else if (read == -1) {
-                break
-            } else {
-                break
-            }
-        }
-        val result = String(outbuf.toByteArray())
-        httpConnectionStream.close()
-        outbuf.close()
-        return result
-    }
-
 }
