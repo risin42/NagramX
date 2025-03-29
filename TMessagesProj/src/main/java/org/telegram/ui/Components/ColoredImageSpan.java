aa07debb8fa12c18ee09f76292162e802b1ccc7c
risin42 <5331402+risin42@users.noreply.github.com>
2025-03-29 14:54:12 +0900

    fix: show Stars drawable

--- TMessagesProj/src/main/java/org/telegram/ui/Components/ColoredImageSpan.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Components/ColoredImageSpan.java
@@ -58,10 +58,8 @@ public class ColoredImageSpan extends ReplacementSpan {
 
     public ColoredImageSpan(Drawable drawable, int verticalAlignment) {
         this.drawable = drawable;
-        this.usePaintColor = true;
         if (drawable != null) {
             drawable.setBounds(0, 0, drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight());
-            drawable.setColorFilter(new PorterDuffColorFilter(drawableColor, PorterDuff.Mode.SRC_IN));
         }
         this.verticalAlignment = verticalAlignment;
     }
@@ -69,15 +67,6 @@ public class ColoredImageSpan extends ReplacementSpan {
     public ColoredImageSpan(Drawable drawable, boolean ignorePaintTextAlpha) {
         this(drawable, ALIGN_DEFAULT);
         this.ignorePaintAlpha = ignorePaintTextAlpha;
-    }
-
-    public ColoredImageSpan(Drawable drawable, int drawableColor, int verticalAlignment) {
-        this.drawable = drawable;
-        this.drawableColor = drawableColor;
-        this.usePaintColor = true;
-        drawable.setBounds(0, 0, drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight());
-        drawable.setColorFilter(new PorterDuffColorFilter(drawableColor, PorterDuff.Mode.SRC_IN));
-        this.verticalAlignment = verticalAlignment;
     }
 
     private boolean isRelativeSize;
