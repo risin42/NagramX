be66a4a31fe02b211c69b4b8a395ea2fee92d670
risin42 <5331402+risin42@users.noreply.github.com>
2025-04-14 22:24:08 +0900

    fix: remove non-existent system accounts

--- TMessagesProj/src/main/java/org/telegram/messenger/ContactsController.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/ContactsController.java
@@ -397,6 +397,35 @@ public class ContactsController extends BaseController {
         systemAccount = null;
         Utilities.globalQueue.postRunnable(() -> {
             AccountManager am = AccountManager.get(ApplicationLoader.applicationContext);
+            try {
+                Account[] accounts = am.getAccountsByType(BuildConfig.APPLICATION_ID);
+                for (int a = 0; a < accounts.length; a++) {
+                    Account acc = accounts[a];
+                    boolean found = false;
+                    for (int b = 0; b < UserConfig.MAX_ACCOUNT_COUNT; b++) {
+                        TLRPC.User user = UserConfig.getInstance(b).getCurrentUser();
+                        if (user != null) {
+                            if (acc.name.equals(formatName(user.first_name, user.last_name))) {
+                                if (b == currentAccount) {
+                                    systemAccount = acc;
+                                }
+                                found = true;
+                                break;
+                            }
+                        }
+                    }
+                    if (!found) {
+                        try {
+                            am.removeAccount(accounts[a], null, null);
+                        } catch (Exception ignore) {
+
+                        }
+                    }
+
+                }
+            } catch (Throwable ignore) {
+
+            }
             if (getUserConfig().isClientActivated()) {
                 readContacts();
                 if (systemAccount == null && !NekoConfig.disableSystemAccount.Bool()) {
@@ -404,8 +433,8 @@ public class ContactsController extends BaseController {
                         TLRPC.User user = getUserConfig().getCurrentUser();
                         systemAccount = new Account(formatName(user.first_name, user.last_name), BuildConfig.APPLICATION_ID);
                         am.addAccountExplicitly(systemAccount, "", null);
-                    } catch (Exception e) {
-                        FileLog.e(e);
+                    } catch (Exception ignore) {
+
                     }
                 }
             }
