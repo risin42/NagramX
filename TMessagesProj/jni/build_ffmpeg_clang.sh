a44ae3f30271f2c5b032ba5f6df3e9d597c31fe1
risin42 <5331402+risin42@users.noreply.github.com>
2025-05-07 21:11:08 +0900

    fix: av1 decoding

--- TMessagesProj/jni/build_ffmpeg_clang.sh
+++ TMessagesProj/jni/build_ffmpeg_clang.sh
@@ -19,8 +19,8 @@ function build_one {
 	CXX=${CC_PREFIX}clang++
 	CROSS_PREFIX=${PREBUILT}/bin/${ARCH_NAME}-linux-${BIN_MIDDLE}-
 	
-	INCLUDES=" -I./${LIBVPXPREFIX}/include"
-	LIBS=" -L./${LIBVPXPREFIX}/lib"
+	INCLUDES=" -I./${LIBVPXPREFIX}/include -I${LIBDAV1DPREFIX}/include"
+	LIBS=" -L./${LIBVPXPREFIX}/lib -L${LIBDAV1DPREFIX}/lib"
 
 	echo "Cleaning..."
 	rm -f config.h
@@ -28,7 +28,7 @@ function build_one {
 
 	echo "Configuring..."
 
-	./configure \
+	PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:${LIBDAV1DPREFIX}/lib/pkgconfig ./configure \
 	--nm=${NM} \
 	--ar=${AR} \
 	--strip=${STRIP} \
@@ -40,6 +40,8 @@ function build_one {
 	--enable-cross-compile \
 	--x86asmexe=$NDK/prebuilt/${BUILD_PLATFORM}/bin/yasm \
 	--prefix=$PREFIX \
+	--pkg-config="pkg-config" \
+	--pkg-config-flags="--static" \
 	--enable-pic \
 	--disable-shared \
 	--enable-static \
@@ -79,6 +81,10 @@ function build_one {
   --enable-muxer=matroska \
   --enable-bsf=vp9_superframe \
   --enable-bsf=vp9_raw_reorder \
+  \
+  --enable-libdav1d \
+  --enable-decoder=libdav1d \
+  --enable-decoder=av1 \
 	--enable-runtime-cpudetect \
 	--enable-pthreads \
 	--enable-avresample \
@@ -175,6 +181,7 @@ function build {
 				CPU=x86_64
 				PREFIX=./build/$CPU
 				LIBVPXPREFIX=../libvpx/build/x86_64
+				LIBDAV1DPREFIX=$PREFIX_D/dav1d/build/x86_64
 				ADDITIONAL_CONFIGURE_FLAG="--disable-asm"
 				build_one
 			;;
@@ -191,6 +198,7 @@ function build {
 				OPTIMIZE_CFLAGS=
 				PREFIX=./build/$CPU
 				LIBVPXPREFIX=../libvpx/build/arm64-v8a
+				LIBDAV1DPREFIX=$PREFIX_D/dav1d/build/arm64-v8a
 				ADDITIONAL_CONFIGURE_FLAG="--enable-neon --enable-optimizations"
 				build_one
 			;;
@@ -207,6 +215,7 @@ function build {
 				OPTIMIZE_CFLAGS="-marm -march=$CPU"
 				PREFIX=./build/armeabi-v7a
 				LIBVPXPREFIX=../libvpx/build/armeabi-v7a
+				LIBDAV1DPREFIX=$PREFIX_D/dav1d/build/armeabi-v7a
 				ADDITIONAL_CONFIGURE_FLAG="--enable-neon"
 				build_one
 			;;
@@ -223,6 +232,7 @@ function build {
 				OPTIMIZE_CFLAGS="-march=$CPU"
 				PREFIX=./build/x86
 				LIBVPXPREFIX=../libvpx/build/x86
+				LIBDAV1DPREFIX=$PREFIX_D/dav1d/build/x86
 				ADDITIONAL_CONFIGURE_FLAG="--disable-x86asm --disable-inline-asm --disable-asm"
 				build_one
 			;;
