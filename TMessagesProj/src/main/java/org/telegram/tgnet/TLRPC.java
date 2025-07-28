df13cde54d4008b76d61374676da350c61e0b59a
risin42 <5331402+risin42@users.noreply.github.com>
2025-07-28 11:59:46 +0900

    chore: remove bot login

--- TMessagesProj/src/main/java/org/telegram/tgnet/TLRPC.java
+++ TMessagesProj/src/main/java/org/telegram/tgnet/TLRPC.java
@@ -53389,27 +53389,6 @@ public class TLRPC {
         }
     }
 
-    public static class TL_auth_importBotAuthorization extends TLObject {
-        public static int constructor = 0x67a3ff2c;
-
-        public int flags;
-        public int api_id;
-        public String api_hash;
-        public String bot_auth_token;
-
-        public TLObject deserializeResponse(InputSerializedData  stream, int constructor, boolean exception) {
-            return TL_auth_authorization.TLdeserialize(stream, constructor, exception);
-        }
-
-        public void serializeToStream(OutputSerializedData  stream) {
-            stream.writeInt32(constructor);
-            stream.writeInt32(flags);
-            stream.writeInt32(api_id);
-            stream.writeString(api_hash);
-            stream.writeString(bot_auth_token);
-        }
-    }
-
     public static class TL_auth_exportLoginToken extends TLObject {
         public static final int constructor = 0xb7e085fe;
 
