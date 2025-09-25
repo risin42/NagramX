f2054597abc44420ea7f3dffc25ae580ade2e8a1
risin42 <5331402+risin42@users.noreply.github.com>
2025-09-25 13:12:15 +0900

    chore: disable dynamic pack order

--- TMessagesProj/src/main/java/org/telegram/messenger/SharedConfig.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/SharedConfig.java
@@ -270,7 +270,7 @@ public class SharedConfig {
     public static boolean storiesIntroShown;
     public static boolean disableVoiceAudioEffects;
     public static boolean forceDisableTabletMode;
-    public static boolean updateStickersOrderOnSend = false;
+    public static boolean updateStickersOrderOnSend;
     public static boolean bigCameraForRound;
     public static Boolean useCamera2Force;
     public static boolean useNewBlur;
@@ -696,7 +696,7 @@ public class SharedConfig {
             dontAskManageStorage = preferences.getBoolean("dontAskManageStorage", false);
             hasEmailLogin = preferences.getBoolean("hasEmailLogin", false);
             isFloatingDebugActive = preferences.getBoolean("floatingDebugActive", false);
-            updateStickersOrderOnSend = preferences.getBoolean("updateStickersOrderOnSend", true);
+            updateStickersOrderOnSend = preferences.getBoolean("updateStickersOrderOnSend", false);
             dayNightWallpaperSwitchHint = preferences.getInt("dayNightWallpaperSwitchHint", 0);
             bigCameraForRound = preferences.getBoolean("bigCameraForRound", false);
             useNewBlur = preferences.getBoolean("useNewBlur", true);
