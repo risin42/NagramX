ecddf40316ab02ba5bbefb6cb804583902b7398c
risin42 <5331402+risin42@users.noreply.github.com>
2025-11-26 15:23:05 +0900

    chore: hide empty star

--- TMessagesProj/src/main/java/org/telegram/ui/Components/Reactions/ReactionsLayoutInBubble.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Components/Reactions/ReactionsLayoutInBubble.java
@@ -210,7 +210,7 @@ public class ReactionsLayoutInBubble {
                             }
                         }
                     }
-                    if (!hasPaidReaction) {
+                    if (false && !hasPaidReaction) {
                         includeEmptyStarButton = true;
                     }
                 }
