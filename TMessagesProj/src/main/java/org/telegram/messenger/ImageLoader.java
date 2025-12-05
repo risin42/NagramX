22f6495a46f20e720435630f7180afc0c6112cad
risin42 <5331402+risin42@users.noreply.github.com>
2025-12-05 23:28:13 +0900

    fix: save deleted media from cache

--- TMessagesProj/src/main/java/org/telegram/messenger/ImageLoader.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/ImageLoader.java
@@ -1698,6 +1698,16 @@ public class ImageLoader {
 
         private void onPostExecute(final Drawable drawable) {
             AndroidUtilities.runOnUIThread(() -> {
+                // save deleted media from cache
+                if (NaConfig.INSTANCE.getEnableSaveDeletedMessages().Bool() && cacheImage.finalFilePath != null && cacheImage.parentObject instanceof MessageObject messageObject) {
+                    if (messageObject.messageOwner != null && messageObject.messageOwner.ayuDeleted) {
+                        String fileName = cacheImage.finalFilePath.getName();
+                        if (fileName.endsWith(".jpg") || fileName.endsWith(".mp4")) {
+                            NotificationCenter.getInstance(cacheImage.currentAccount).postNotificationName(AyuConstants.DELETED_MEDIA_LOADED_NOTIFICATION, fileName, cacheImage.finalFilePath);
+                        }
+                    }
+                }
+
                 Drawable toSet = null;
                 String decrementKey = null;
                 if (drawable instanceof RLottieDrawable) {
