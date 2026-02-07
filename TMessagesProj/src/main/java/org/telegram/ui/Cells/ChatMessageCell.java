adbec8ea052645baf35d268493fca6542d25fc38
risin42 <5331402+risin42@users.noreply.github.com>
2026-02-07 22:07:56 +0900

    fix: crash when opening blocked Replies Bot 865e4b9b9a:2557

--- TMessagesProj/src/main/java/org/telegram/ui/Cells/ChatMessageCell.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Cells/ChatMessageCell.java
@@ -5226,7 +5226,7 @@ public class ChatMessageCell extends BaseCell implements SeekBar.SeekBarDelegate
             invalidate();
         }
 
-        if (currentMessageObject != null && currentMessageObject.textLayoutBlocks != null || needHide) {
+        if (currentMessageObject != null && currentMessageObject.textLayoutBlocks != null && !needHide) {
             int p = position - textY;
 
             int newFirst = -1, newLast = -1, newCount = 0;
